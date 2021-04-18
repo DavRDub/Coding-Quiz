dc374726ebd76fe5edfff0cc970a5d23c33d56e9
David Whitesel <whiteseldavid@gmail.com>
2021-04-18 16:39:45 -0700

    Adding questions.js and highscore.js

---
+++ assets/questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        question: "Which fully evolved starter Pokemon originated from the Kanto Region?:",
+        options: ["Incineroar", "Meganium", "Empoleon", "Charizard"],
+        answer: "Charizard"
+    },
+    {
+        question: "Which evil group tried to 'liberate' all Pokemon from humans?",
+        options: ["Team Plasma", "Team Galactic", "Team Skull", "Team Rocket"],
+        answer: "Team Plasma"
+    },
+    {
+        question: "______ is legendary Pokemon Mewtwo's signature move.",
+        options: ["Aura Sphere", "Psystrike", "Psychic", "Shadow Ball"],
+        answer: "Psystrike"
+    },
+    {
+        question: "Who is the main Hero/Heroine of the game Pokemon Black/White?",
+        options: ["Calem/Serena", "Brendan/May", "Red/Green", "Hilbert/Hilda"],
+        answer: "Hilbert/Hilda"
+    },
+    {
+        question: "Which Pokemon region did not have gym badges?",
+        options: ["Galar", "Sinnoh", "Alola", "Kanto"],
+        answer: "Alola"
+    },
+    {
+        question: "Which Pokemon is first seen using Mega Evolution in the game Pokemon X/Y?",
+        options: ["Lucario", "Salamence", "Gardevoir", "Scizor"],
+        answer: "Lucario"
+    }
+]
